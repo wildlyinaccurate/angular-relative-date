9ba8ecf932121807af25f759ec02ac395c96d988
Nick Doiron <ndoiron@mapmeld.com>
2015-01-19 00:07:21 +0000

    make translate-able

--- angular-relative-date.coffee
+++ angular-relative-date.coffee
@@ -1,8 +1,42 @@
 'use strict'
 
-angular.module('relativeDate', [])
+angular.module('relativeDate', ['pascalprecht.translate'])
+  .config(['$translateProvider', ($translateProvider) ->
+    $translateProvider.translations('en', {
+      just_now: 'just now',
+      seconds_ago: '{{seconds}} seconds ago',
+      a_minute_ago: 'a minute ago',
+      minutes_ago: '{{minutes}} minutes ago',
+      an_hour_ago: 'an hour ago',
+      hours_ago: '{{hours}} hours ago',
+      yesterday: 'yesterday',
+      days_ago: '{{days}} days ago',
+      a_week_ago: 'a week ago',
+      weeks_ago: '{{weeks}} weeks ago',
+      a_month_ago: 'a month ago',
+      months_ago: '{{months}} months ago',
+      a_year_ago: 'a year ago',
+      years_ago: '{{years}} years ago',
+      over_a_year_ago: 'over a year ago',
+      seconds_from_now: '{{seconds}} seconds from now',
+      a_minute_from_now: 'a minute from now',
+      minutes_from_now: '{{minutes}} minutes from now',
+      an_hour_from_now: 'an hour from now',
+      hours_from_now: '{{hours}} hours from now',
+      tomorrow: 'tomorrow',
+      days_from_now: '{{days}} days from now',
+      a_week_from_now: 'a week from now',
+      weeks_from_now: '{{weeks}} weeks from now',
+      a_month_from_now: 'a month from now',
+      months_from_now: '{{months}} months from now',
+      a_year_from_now: 'a year from now',
+      years_from_now: '{{years}} years from now',
+      over_a_year_from_now: 'over a year from now'
+    })
+    .preferredLanguage('en')
+  ])
   .value('now', new Date())
-  .filter 'relativeDate', ['now', (now) ->
+  .filter 'relativeDate', ['$filter', 'now', ($filter, now) ->
     (date) ->
       date = new Date(date) unless date instanceof Date
       delta = null
@@ -26,33 +60,59 @@ angular.module('relativeDate', [])
 
       if now >= date
         switch
-          when delta < 30 then 'just now'
-          when delta < minute then "#{delta} seconds ago"
-          when delta < 2 * minute then 'a minute ago'
-          when delta < hour then "#{Math.floor(delta / minute)} minutes ago"
-          when Math.floor(delta / hour) == 1 then 'an hour ago'
-          when delta < day then "#{Math.floor(delta / hour)} hours ago"
-          when delta < day * 2 then 'yesterday'
-          when delta < week then "#{Math.floor(delta / day)} days ago"
-          when Math.floor(delta / week) == 1 then 'a week ago'
-          when delta < month then "#{Math.floor(delta / week)} weeks ago"
-          when Math.floor(delta / month) == 1 then 'a month ago'
-          when delta < year then "#{Math.floor(delta / month)} months ago"
-          when Math.floor(delta / year) == 1 then 'a year ago'
-          else 'over a year ago'
+          when delta < 30 then translatePhrase = 'just_now'
+          when delta < minute
+            translatePhrase = 'seconds_ago'
+            translateArg = { seconds: delta }
+          when delta < 2 * minute then translatePhrase = 'a_minute_ago'
+          when delta < hour
+            translatePhrase = 'minutes_ago'
+            translateArg = { minutes: Math.floor(delta / minute) }
+          when Math.floor(delta / hour) == 1 then translatePhrase = 'an_hour_ago'
+          when delta < day
+            translatePhrase = 'hours_ago'
+            translateArg = { hours: Math.floor(delta / hour) }
+          when delta < day * 2 then translatePhrase = 'yesterday'
+          when delta < week
+            translatePhrase = 'days_ago'
+            translateArg = { days: Math.floor(delta / day) }
+          when Math.floor(delta / week) == 1 then translatePhrase = 'a_week_ago'
+          when delta < month
+            translatePhrase = 'weeks_ago'
+            translateArg = { weeks: Math.floor(delta / week) }
+          when Math.floor(delta / month) == 1 then translatePhrase = 'a_month_ago'
+          when delta < year
+            translatePhrase = 'months_ago'
+            translateArg = { months: Math.floor(delta / month) }
+          when Math.floor(delta / year) == 1 then translatePhrase = 'a_year_ago'
+          else translatePhrase = 'over_a_year_ago'
       else
         switch
-          when delta < minute then "#{delta} seconds from now"
-          when delta < 2 * minute then 'a minute from now'
-          when delta < hour then "#{Math.floor(delta / minute)} minutes from now"
-          when Math.floor(delta / hour) == 1 then 'an hour from now'
-          when delta < day then "#{Math.floor(delta / hour)} hours from now"
-          when delta < day * 2 then 'tomorrow'
-          when delta < week then "#{Math.floor(delta / day)} days from now"
-          when Math.floor(delta / week) == 1 then 'a week from now'
-          when delta < month then "#{Math.floor(delta / week)} weeks from now"
-          when Math.floor(delta / month) == 1 then 'a month from now'
-          when delta < year then "#{Math.floor(delta / month)} months from now"
-          when Math.floor(delta / year) == 1 then 'a year from now'
-          else 'over a year from now'
+          when delta < minute
+            translatePhrase = 'seconds_from_now'
+            translateArg = { seconds: delta }
+          when delta < 2 * minute then translatePhrase = 'a_minute_from_now'
+          when delta < hour
+            translatePhrase = 'minutes_from_now'
+            translateArg = { minutes: Math.floor(delta / minute) }
+          when Math.floor(delta / hour) == 1 then translatePhrase = 'an_hour_from_now'
+          when delta < day
+            translatePhrase = 'hours_from_now'
+            translateArg = { hours: Math.floor(delta / hour) }
+          when delta < day * 2 then translatePhrase = 'tomorrow'
+          when delta < week
+            translatePhrase = 'days_from_now'
+            translateArg = { days: Math.floor(delta / day) }
+          when Math.floor(delta / week) == 1 then translatePhrase = 'a_week_from_now'
+          when delta < month
+            translatePhrase = 'weeks_from_now'
+            translateArg = { weeks: Math.floor(delta / week) }
+          when Math.floor(delta / month) == 1 then translatePhrase = 'a_month_from_now'
+          when delta < year
+            translatePhrase = 'months_from_now'
+            translateArg = { months: Math.floor(delta / month) }
+          when Math.floor(delta / year) == 1 then translatePhrase = 'a_year_from_now'
+          else translatePhrase = 'over_a_year_from_now'
+
+      $filter('translate')(translatePhrase, translateArg)
   ]
--- angular-relative-date.js
+++ angular-relative-date.js
@@ -1,9 +1,43 @@
 (function() {
   'use strict';
-  angular.module('relativeDate', []).value('now', new Date()).filter('relativeDate', [
-    'now', function(now) {
+  angular.module('relativeDate', ['pascalprecht.translate']).config([
+    '$translateProvider', function($translateProvider) {
+      return $translateProvider.translations('en', {
+        just_now: 'just now',
+        seconds_ago: '{{seconds}} seconds ago',
+        a_minute_ago: 'a minute ago',
+        minutes_ago: '{{minutes}} minutes ago',
+        an_hour_ago: 'an hour ago',
+        hours_ago: '{{hours}} hours ago',
+        yesterday: 'yesterday',
+        days_ago: '{{days}} days ago',
+        a_week_ago: 'a week ago',
+        weeks_ago: '{{weeks}} weeks ago',
+        a_month_ago: 'a month ago',
+        months_ago: '{{months}} months ago',
+        a_year_ago: 'a year ago',
+        years_ago: '{{years}} years ago',
+        over_a_year_ago: 'over a year ago',
+        seconds_from_now: '{{seconds}} seconds from now',
+        a_minute_from_now: 'a minute from now',
+        minutes_from_now: '{{minutes}} minutes from now',
+        an_hour_from_now: 'an hour from now',
+        hours_from_now: '{{hours}} hours from now',
+        tomorrow: 'tomorrow',
+        days_from_now: '{{days}} days from now',
+        a_week_from_now: 'a week from now',
+        weeks_from_now: '{{weeks}} weeks from now',
+        a_month_from_now: 'a month from now',
+        months_from_now: '{{months}} months from now',
+        a_year_from_now: 'a year from now',
+        years_from_now: '{{years}} years from now',
+        over_a_year_from_now: 'over a year from now'
+      }).preferredLanguage('en');
+    }
+  ]).value('now', new Date()).filter('relativeDate', [
+    '$filter', 'now', function($filter, now) {
       return function(date) {
-        var calculateDelta, day, delta, hour, minute, month, week, year;
+        var calculateDelta, day, delta, hour, minute, month, translateArg, translatePhrase, week, year;
         if (!(date instanceof Date)) {
           date = new Date(date);
         }
@@ -25,64 +59,126 @@
         if (now >= date) {
           switch (false) {
             case !(delta < 30):
-              return 'just now';
+              translatePhrase = 'just_now';
+              break;
             case !(delta < minute):
-              return "" + delta + " seconds ago";
+              translatePhrase = 'seconds_ago';
+              translateArg = {
+                seconds: delta
+              };
+              break;
             case !(delta < 2 * minute):
-              return 'a minute ago';
+              translatePhrase = 'a_minute_ago';
+              break;
             case !(delta < hour):
-              return "" + (Math.floor(delta / minute)) + " minutes ago";
+              translatePhrase = 'minutes_ago';
+              translateArg = {
+                minutes: Math.floor(delta / minute)
+              };
+              break;
             case Math.floor(delta / hour) !== 1:
-              return 'an hour ago';
+              translatePhrase = 'an_hour_ago';
+              break;
             case !(delta < day):
-              return "" + (Math.floor(delta / hour)) + " hours ago";
+              translatePhrase = 'hours_ago';
+              translateArg = {
+                hours: Math.floor(delta / hour)
+              };
+              break;
             case !(delta < day * 2):
-              return 'yesterday';
+              translatePhrase = 'yesterday';
+              break;
             case !(delta < week):
-              return "" + (Math.floor(delta / day)) + " days ago";
+              translatePhrase = 'days_ago';
+              translateArg = {
+                days: Math.floor(delta / day)
+              };
+              break;
             case Math.floor(delta / week) !== 1:
-              return 'a week ago';
+              translatePhrase = 'a_week_ago';
+              break;
             case !(delta < month):
-              return "" + (Math.floor(delta / week)) + " weeks ago";
+              translatePhrase = 'weeks_ago';
+              translateArg = {
+                weeks: Math.floor(delta / week)
+              };
+              break;
             case Math.floor(delta / month) !== 1:
-              return 'a month ago';
+              translatePhrase = 'a_month_ago';
+              break;
             case !(delta < year):
-              return "" + (Math.floor(delta / month)) + " months ago";
+              translatePhrase = 'months_ago';
+              translateArg = {
+                months: Math.floor(delta / month)
+              };
+              break;
             case Math.floor(delta / year) !== 1:
-              return 'a year ago';
+              translatePhrase = 'a_year_ago';
+              break;
             default:
-              return 'over a year ago';
+              translatePhrase = 'over_a_year_ago';
           }
         } else {
           switch (false) {
             case !(delta < minute):
-              return "" + delta + " seconds from now";
+              translatePhrase = 'seconds_from_now';
+              translateArg = {
+                seconds: delta
+              };
+              break;
             case !(delta < 2 * minute):
-              return 'a minute from now';
+              translatePhrase = 'a_minute_from_now';
+              break;
             case !(delta < hour):
-              return "" + (Math.floor(delta / minute)) + " minutes from now";
+              translatePhrase = 'minutes_from_now';
+              translateArg = {
+                minutes: Math.floor(delta / minute)
+              };
+              break;
             case Math.floor(delta / hour) !== 1:
-              return 'an hour from now';
+              translatePhrase = 'an_hour_from_now';
+              break;
             case !(delta < day):
-              return "" + (Math.floor(delta / hour)) + " hours from now";
+              translatePhrase = 'hours_from_now';
+              translateArg = {
+                hours: Math.floor(delta / hour)
+              };
+              break;
             case !(delta < day * 2):
-              return 'tomorrow';
+              translatePhrase = 'tomorrow';
+              break;
             case !(delta < week):
-              return "" + (Math.floor(delta / day)) + " days from now";
+              translatePhrase = 'days_from_now';
+              translateArg = {
+                days: Math.floor(delta / day)
+              };
+              break;
             case Math.floor(delta / week) !== 1:
-              return 'a week from now';
+              translatePhrase = 'a_week_from_now';
+              break;
             case !(delta < month):
-              return "" + (Math.floor(delta / week)) + " weeks from now";
+              translatePhrase = 'weeks_from_now';
+              translateArg = {
+                weeks: Math.floor(delta / week)
+              };
+              break;
             case Math.floor(delta / month) !== 1:
-              return 'a month from now';
+              translatePhrase = 'a_month_from_now';
+              break;
             case !(delta < year):
-              return "" + (Math.floor(delta / month)) + " months from now";
+              translatePhrase = 'months_from_now';
+              translateArg = {
+                months: Math.floor(delta / month)
+              };
+              break;
             case Math.floor(delta / year) !== 1:
-              return 'a year from now';
+              translatePhrase = 'a_year_from_now';
+              break;
             default:
-              return 'over a year from now';
+              translatePhrase = 'over_a_year_from_now';
           }
         }
+        return $filter('translate')(translatePhrase, translateArg);
       };
     }
   ]);
